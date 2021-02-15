63aa9c24ac086e791bb4fe49ef17b8e260b2bb3d
Leah Scott-Zechlin <scottzec@gmail.com>
2021-02-15 08:19:06 -0800

    returns the assigned regions of signed-in user

--- src/WeatherResponse.js
+++ src/WeatherResponse.js
@@ -7,38 +7,19 @@ import ls from 'local-storage';
 
   // FILTER FOR REGIONS THAT HAVE THE USER_ID OF THE USERNAME
   // THEN DISPLAY THEM
-// const WeatherResponse = (props) => { 
-//   const fullUrl = 'http://127.0.0.1:5000/api/regions/<user_id>'; //filter by user_id
-
-//   const [regions, setRegions] = useState([]);
-//   const [errors, setErrors] = useState(null);
-
-//   useEffect (() => {
-//     axios.get(fullUrl)
-//     .then((response) => {
-//       const weatherList = response.data;
-//       console.log(weatherList);
-//       setRegions(weatherList);
-//     })
-//     .catch((error) => {
-//       setErrors(error.message);
-//       console.log(error.message);
-//     })
-//   }, []);
-
-
 const WeatherResponse = (props) => { 
-  const fullUrl = 'http://127.0.0.1:5000/'; //WELCOME
+  const fullUrl = `http://127.0.0.1:5000/api/regions/${props.userID}`; 
+  //filter by user_id
 
-  const [regions, setRegions] = useState([]);
+  const [userRegions, setUserRegions] = useState([]);
   const [errors, setErrors] = useState(null);
 
   useEffect (() => {
     axios.get(fullUrl)
     .then((response) => {
-      const weatherList = response.data;
-      console.log(weatherList);
-      setRegions(weatherList);
+      const regionsList = response.data;
+      console.log(regionsList);
+      setUserRegions(regionsList);
     })
     .catch((error) => {
       setErrors(error.message);
@@ -49,15 +30,54 @@ const WeatherResponse = (props) => {
   return (
     <div className='weatherRegions'>
       <p>{props.userID}</p>
+      {/* <p>{regions}</p> */}
       <ul>
-        {regions.map( (region) => {
-          return (<li className="no_bullet" key={region.id}>{<WeatherRegion id={region.id} day={region.day} icon={region.icon} region={region.region} temp={region.temp} weather={region.weather} />}</li>);
+        {userRegions.map( (region) => {
+        return (<li className="no_bullet" key={region.id}>{region.region_name}</li>);
+        // return (<li className="no_bullet" key={region.id}>{<WeatherRegion id={region.id} day={region.day} icon={region.icon} region={region.region} temp={region.temp} weather={region.weather} />}</li>);
           })
         }
       </ul>
     </div>
   )
 };
+
+
+// const WeatherResponse = (props) => { 
+//   const fullUrl = 'http://127.0.0.1:5000/'; //WELCOME
+
+//   const [regions, setRegions] = useState([]);
+//   const [errors, setErrors] = useState(null);
+
+//   useEffect (() => {
+//     axios.get(fullUrl)
+//     .then((response) => {
+//       const weatherList = response.data;
+//       console.log(weatherList);
+//       console.log("Here come regions")
+//       console.log(regions)
+//       setRegions(weatherList);
+//       console.log(regions)
+//     })
+//     .catch((error) => {
+//       setErrors(error.message);
+//       console.log(error.message);
+//     })
+//   }, []);
+
+//   return (
+//     <div className='weatherRegions'>
+//       <p>{props.userID}</p>
+//       {/* <p>{regions}</p> */}
+//       <ul>
+//         {regions.map( (region) => {
+//           return (<li className="no_bullet" key={region.id}>{<WeatherRegion id={region.id} day={region.day} icon={region.icon} region={region.region} temp={region.temp} weather={region.weather} />}</li>);
+//           })
+//         }
+//       </ul>
+//     </div>
+//   )
+// };
 
 WeatherResponse.propTypes = {
   userID: PropTypes.number
